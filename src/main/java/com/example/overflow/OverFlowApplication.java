8be975379a20761216058deec4fe47ee28ff1707
sayya <djdj9876@naver.com>
2023-09-12 16:12:05 +0900

    Project init

---
+++ src/main/java/com/example/overflow/OverFlowApplication.java
@@ -0,0 +1,13 @@
+package com.example.overflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OverFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OverFlowApplication.class, args);
+    }
+
+}
